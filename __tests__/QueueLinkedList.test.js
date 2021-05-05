2a18d7ef5da58a79494d8e6092bdc2d31a29f660
alejo4373 <alejandrofranco@pursuit.org>
2021-05-05 17:58:41 -0400

    Adjust tests descriptions

--- __tests__/QueueLinkedList.test.js
+++ __tests__/QueueLinkedList.test.js
@@ -1,25 +1,25 @@
 const Queue = require("../QueueLinkedList")
 
 describe("Queue functionality", () => {
-  test("enqueue should add an element to queue", () => {
+  test(".enqueue() adds an element to queue", () => {
     const testQ = new Queue() // Arrange/setup
     testQ.enqueue(100) // act
     expect(testQ.peek()).toBe(100) // assert
   })
 
-  test("dequeue should remove an element from the front of the queue", () => {
+  test(".dequeue() removes an element from the front of the queue", () => {
     const testQ = new Queue() // Arrange/setup
     const num = 100
     testQ.enqueue(num)
     expect(testQ.dequeue()).toBe(num)
   })
 
-  test("dequeue should throw an error if queue is empty", () => {
+  test(".dequeue() returns undefined if queue is empty", () => {
     const testQ = new Queue()
-    expect(() => testQ.dequeue()).toThrow("Nothing to dequeue")
+    expect(testQ.dequeue()).toBe(undefined)
   })
 
-  test("peek returns element in the front of the queue and does not remove it", () => {
+  test(".peek() returns element in the front of the queue and does not remove it", () => {
     const testQ = new Queue()
     const num = 100
     testQ.enqueue(num)
@@ -27,7 +27,7 @@ describe("Queue functionality", () => {
     expect(testQ.peek()).toBe(100)
   })
 
-  test("isEmpty checks if the queue is empty", () => {
+  test(".isEmpty() returns true if the queue is empty and false otherwise", () => {
     const testQA = new Queue()
     testQA.enqueue(100)
     expect(testQA.isEmpty()).toBe(false)
