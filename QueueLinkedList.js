f899482e4ea5be92928c7fb4f740e7e0fdcf5cc9
alejo4373 <alejandrofranco@pursuit.org>
2021-05-05 19:06:15 -0400

    Add QueueLinkedList started code

--- QueueLinkedList.js
+++ QueueLinkedList.js
@@ -1,3 +1,17 @@
+/*
+  Use the Node class for the nodes of your linked list
+*/
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+/*
+  QueueLinkedList will be linked lists with head, tail and length and it will have
+  the Queue API methods
+*/
 class QueueLinkedList {
 
 }
